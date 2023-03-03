f2c55dd873d560c8f2d091475d86756340efa26a
James Caleca <jacaleca2@gmail.com>
2023-03-02 18:05:09 -0700

    CSS changes in contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,48 +1,8 @@
 import React, { useRef } from 'react'
-// import { useHistory } from 'react-router-dom'
-// import axios from 'axios'
-// import Navbar from './Navbar'
 import emailjs from '@emailjs/browser';
 import '../styles.css'
 
 function Contact() {
-    // const initInputs = 
-    // {
-    //     name: '',
-    //     email: '',
-    //     message: ''
-    // }
-
-    // const [inputs, setInputs] = useState(initInputs)
-    // const [contacts, setContacts] = useState([])
-    // const history = useHistory()
-
-    // function submitBtnRedirect() {
-    //     history.push('/')
-    //     alert("Thank you for submitting the form! I'll look forward to speaking with you soon.")
-    // }
-
-    // function handleChange(e) {
-    //     const { name, value } = e.target
-    //     setInputs(prevInputs => ({...prevInputs, [name]: value}))
-    //     console.log('inputs', inputs)
-    // }
-
-    // function handleSubmit(e) {
-    //     e.preventDefault()
-    //     setInputs(initInputs)
-    //     submitBtnRedirect()
-    // }
-
-    // function postContact(newContact) {
-    //     console.log('post new contact message', newContact)
-    //     axios.post('/contact', newContact)
-    //         .then(res => {
-    //             setContacts(prevContacts => [...prevContacts, res.data])
-    //         })
-    //         .catch(err => console.log(err))
-    // }
-
     const form = useRef();
 
     const sendEmail = (e) => {
@@ -64,15 +24,30 @@ function Contact() {
 
     return (
         <>
-            <form ref={form} onSubmit={sendEmail}>
-                <label>Name-</label>
-                <input type="text" name="user_name" />
-                <label>Email</label>
-                <input type="email" name="user_email" />
-                <label>Message</label>
-                <textarea name="message" />
-                <input type="submit" value="Send" />
-            </form>
+          <div className="full-screen-container">
+            <div className="login-container">
+              <h1 className="login-title">Get in contact with me</h1>
+              <form className="form" ref={form} onSubmit={sendEmail}>
+
+                <div className="input-group">
+                  <label for="user_name">Name</label>
+                  <input type="text" name="user_name" id="user_name"/>
+                </div>
+
+                <div className="input-group success">
+                  <label for="user_email">Email</label>
+                  <input type="email" name="user_email" />
+                  <span className="msg">Valid email</span>
+                </div>
+
+                <div className="input-group error">
+                  <label>Message</label>
+                  <textarea name="message" />
+                  <input className="send-button" type="submit" value="Send" />
+                </div>
+              </form>
+            </div>
+          </div>
         </>
     )
 }
